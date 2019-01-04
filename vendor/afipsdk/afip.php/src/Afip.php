1ef26c0a1e11b1e29548e4d3a58959dd56e0306b
Omar Virili <omar.virili@gmail.com>
2019-01-03 23:03:31 -0300

    adiccion

--- vendor/afipsdk/afip.php/src/Afip.php
+++ vendor/afipsdk/afip.php/src/Afip.php
@@ -76,7 +76,7 @@ class Afip {
 	 * @var array[string]
 	 **/
 	var $implemented_ws = array(
-		'FacturacionDetalle',
+		'ExtendedElectronicBilling',
 		'ElectronicBilling',
 		'RegisterScopeFour',
 		'RegisterScopeFive',
